e30829e3a9797b3dc2248ba682e026dde4fd5a17
Andreas Stenlund <a.stenlund@gmail.com>
2020-09-09 18:00:47 +0200

    Add connect button and status label
    Also, bind them to viewmodel properties.

---
+++ ActionCommand.cs
@@ -0,0 +1,76 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+using System;
+using System.Windows.Input;
+
+namespace fs2ff
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action _action;
+        private readonly Func<bool> _predicate;
+
+        public ActionCommand()
+        {
+            _action = () => { };
+            _predicate = () => true;
+        }
+
+        public ActionCommand(Action action)
+        {
+            _action = action;
+            _predicate = () => true;
+        }
+
+        public ActionCommand(Action action, Func<bool> predicate)
+        {
+            _action = action;
+            _predicate = predicate;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? _) => _predicate();
+
+        public void Execute(object? parameter) => _action();
+
+        public void TriggerCanExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+    }
+
+    public class ActionCommand<T> : ICommand where T : struct
+    {
+        private readonly Action<T?> _action;
+        private readonly Func<T?, bool> _predicate;
+
+        public ActionCommand()
+        {
+            _action = _ => { };
+            _predicate = _ => true;
+        }
+
+        public ActionCommand(Action<T?> action)
+        {
+            _action = action;
+            _predicate = _ => true;
+        }
+
+        public ActionCommand(Action<T?> action, Func<T?, bool> predicate)
+        {
+            _action = action;
+            _predicate = predicate;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => parameter is T param ? _predicate(param) : _predicate(null);
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is T param) _action(param);
+            else                      _action(null);
+        }
+
+        public void TriggerCanExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+    }
+}
--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,15 +1,64 @@
 ﻿// ReSharper disable ClassWithVirtualMembersNeverInherited.Global
+// ReSharper disable UnusedMember.Local
 
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Media;
 using fs2ff.Annotations;
 
 namespace fs2ff
 {
     public class MainViewModel : INotifyPropertyChanged, IDisposable
     {
+        private bool _errorOccurred = false;
+
+        public MainViewModel()
+        {
+            ToggleConnectCommand = new ActionCommand(ToggleConnect, CanConnect);
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private enum FlightSimState
+        {
+            Unknown,
+            Connected,
+            Disconnected,
+            ErrorOccurred
+        }
+
+        public string ConnectButtonLabel => Connected ? "Disconnect" : "Connect";
+
+        public Brush StateLabelColor =>
+            CurrentFlightSimState switch
+            {
+                FlightSimState.Connected      => Brushes.Gold,
+                FlightSimState.Disconnected   => Brushes.DarkGray,
+                FlightSimState.ErrorOccurred  => Brushes.OrangeRed,
+                _                             => Brushes.DarkGray
+            };
+
+        public string StateLabelText =>
+            CurrentFlightSimState switch
+            {
+                FlightSimState.Connected      => "CONNECTED",
+                FlightSimState.Disconnected   => "NOT CONNECTED",
+                FlightSimState.ErrorOccurred  => "Unable to connect to Flight Simulator",
+                _                             => ""
+            };
+
+        public ICommand ToggleConnectCommand { get; }
+
+        private bool Connected => false; // TODO: Return Flight Simulator connection status
+
+        private FlightSimState CurrentFlightSimState =>
+            _errorOccurred
+                ? FlightSimState.ErrorOccurred
+                : Connected
+                    ? FlightSimState.Connected
+                    : FlightSimState.Disconnected;
 
         public void Dispose()
         {
@@ -20,6 +69,18 @@ namespace fs2ff
         protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool CanConnect() => false; // TODO: Implement connectability check
+
+        private void Connect() { /* TODO: Connect to Flight Simulator */ }
+
+        private void Disconnect() { /* TODO: Disconnect from Flight Simulator */ }
+
+        private void ToggleConnect()
+        {
+            if (Connected) Disconnect();
+            else              Connect();
         }
     }
 }
