feb7d88b62257908433819439a56f7e9dcd8c77e
Andreas Stenlund <a.stenlund@gmail.com>
2020-09-09 14:59:01 +0200

    Hook up to WM_USER_SIMCONNECT message

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ReSharper disable InconsistentNaming
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace fs2ff
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private const uint WM_USER_SIMCONNECT = 0x0402;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            ((HwndSource) PresentationSource.FromVisual(this)!).AddHook(WndProc);
+        }
+
+        private static IntPtr WndProc(IntPtr hWnd, int iMsg, IntPtr hWParam, IntPtr hLParam, ref bool bHandled)
+        {
+            if (iMsg == WM_USER_SIMCONNECT)
+            {
+                // TODO: Forward message
+            }
+
+            return IntPtr.Zero;
         }
     }
 }
