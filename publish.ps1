5e280af25e2e03829a2f4f514ab3054864dca145
Andreas Stenlund <a.stenlund@gmail.com>
2020-09-13 21:50:08 +0200

    Set version info when building with the publish script

--- publish.ps1
+++ publish.ps1
@@ -3,6 +3,8 @@ param (
     [string]$Configuration = 'Release',
     [ValidateSet('win-x64','win10-x64')]
     [string]$Runtime = 'win-x64',
+    [Parameter(Mandatory)]
+    [string]$Version,
     [switch]$NoClean,
     [switch]$RunAfterBuild
 )
@@ -11,7 +13,7 @@ $PublishDir = ".\bin\$Configuration\netcoreapp3.1\$Runtime\publish"
 
 if (!$NoClean) {
     dotnet clean -r $Runtime -c $Configuration
-    if (Test-Path $PublishDir) { rm $PublishDir -Recurse -Verbose }
+    if (Test-Path $PublishDir) { Remove-Item $PublishDir -Recurse -Verbose }
 }
 
 dotnet restore
@@ -21,10 +23,14 @@ dotnet publish `
     -c $Configuration `
     /p:DebugType=None `
     /p:DebugSymbols=False `
-    /p:PublishSingleFile=true
+    /p:PublishSingleFile=True `
+    /p:Version=$Version
 
-ls $PublishDir
-gi "$PublishDir\fs2ff.exe" | % { "{0:f0} MB`r`n" -f ($_.Length / 1MB) }
+Get-Item "$PublishDir\fs2ff.exe" |
+    Select-Object Name,
+        @{ Name = 'Version'; Expression = { $_.VersionInfo.ProductVersion }},
+        @{ Name = 'Size'; Expression = { "{0:f0} MB`r`n" -f ($_.Length / 1MB) }} |
+    Format-List
 
 if ($RunAfterBuild) {
     & "$PublishDir\fs2ff.exe"
