0a4f78d11a103f4beec7b00eb460bcd79e45ad45
jdamp-wave <jdamp@wavecable.com>
2022-04-02 14:47:30 -0700

    default to 10 AHRS

--- Preferences.Designer.cs
+++ Preferences.Designer.cs
@@ -133,7 +133,7 @@ namespace fs2ff {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("20")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public uint att_freq {
             get {
                 return ((uint)(this["att_freq"]));
